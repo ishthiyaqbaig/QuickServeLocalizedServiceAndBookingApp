e32ec95f4bcac2489281d81374a0e8e598e29c85
M Ishthiyaq Baig <ishthiyaqbaig@gmail.com>
2025-12-11 21:13:57 +0530

    Delete frontend directory

--- frontend/src/services/categoryService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-export const getCategories = async () => {
-    const response = await axios.get("http://localhost:8080/api/service-categories");
-    return response.data;
-};
--- frontend/src/services/customerService.js
+++
@@ -1,8 +0,0 @@
-import apiClient from "../api/client";
-
-export const searchProviders = async (lat, lng, categoryId) => {
-    const res = await apiClient.get('/customer/search', {
-        params: { lat, lng, categoryId }
-    });
-    return res.data;
-};
